e4c143825fa59f00d74e53a060bc26df2e9294b6
arp <irphathil@gmail.com>
2019-05-30 23:47:46 +0200

    Use normal timepoins in x axis

---
+++ README.md
@@ -0,0 +1,8 @@
+# TopChart
+
+Utility scripts to help visualize top log
+
+# Usage
+
+run with:
+"mix run top_graph.exs`
--- top_graph.exs
+++ top_graph.exs
@@ -1,13 +1,13 @@
 #!/usr/bin/env elixir
 
-import Gnuplot
 
 if length(System.argv()) < 1 do
   IO.puts(:stderr, "Usage #{:filename.basename(__ENV__.file())} <toplog.txt>")
   exit 1
 end
+[filename] = System.argv()
 
-{plotdata, timepoints} = TopUtil.parse_file("asd2.txt")
+{plotdata, timepoints} = TopUtil.parse_file(filename)
 IO.inspect(timepoints)
 
 
@@ -20,20 +20,22 @@ end)
 cpu = ds |> Enum.map(fn x -> x |> Enum.map(fn y -> y |> Enum.at(2) end) end)
 _mem = ds |> Enum.map(fn x -> x |> Enum.map(fn y -> y |> Enum.at(3) end) end)
 
-#IO.inspect(pids, limit: :infinity)
-#IO.inspect(cpu, limit: :infinity)
+plots = (for pid <- pids, do: ["-", :using, to_charlist("1:2"), :title, pid])
 
-plots = for pid <- pids, do: ["-", :title, pid, :with, :lines]
-dataset = cpu |> Enum.map(fn x -> Enum.zip(0..length(hd(cpu))-1, x) end)
-#IO.inspect(dataset, limit: :infinity)
+dataset = cpu |> Enum.map(fn x -> 
+  Enum.zip(timepoints, x)
+end)
 
 
 {:ok, _cmd} = Gnuplot.plot([
   [:set, :title, "rand uniform vs normal"],
   [:set, :key, :left, :top],
   [:set, :terminal, "wxt"],
-  #[:set, :timefmt, "%H:%M:%S"],
-  #[:set, :xtics, :format, "%H:%M:%S"],
+  ~w(set format x "%H:%M:%S" timedate)a,
+  [:set, :xdata, :time],
+  [:set, :style, :data, :lines],
+  ~w(set timefmt "%H:%M:%S")a,
   ~w(set grid xtics ytics)a,
   Gnuplot.plots(plots)
   ], dataset)
+
